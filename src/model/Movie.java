da4a20bf301ec07c4b8355cc8c9540b26250425d
Sebastian <sebastian.rivas1991@hotmail.com>
2024-05-07 21:31:44 -0500

    Code Class 02

---
+++ src/model/Movie.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Movie extends Title {
+
+    private String director;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    
+}
